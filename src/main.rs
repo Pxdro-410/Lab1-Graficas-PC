831f9ab444fd618a3aace6d0c6b3f7d675ea5520
Pxdro-410 <pedrojuliocaso@gmail.com>
2026-07-18 20:06:54 -0600

    feat: modificacion general del main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod framebuffer;
+mod line;
+mod bmp;
+
+use crate::bmp::WriteBmp;
+use crate::framebuffer::Framebuffer;
+use crate::line::draw_line;
+
 fn main() {
-    println!("Hello, world!");
+    let mut framebuffer = Framebuffer::new(800, 600);
+
+    framebuffer.set_background_color(0x000000);
+    framebuffer.clear();
+
+    framebuffer.set_current_color(0xFFFFFF);
+
+    let _ = framebuffer.render_buffer("out.bmp");
+
+    println!("Framebuffer rendered to out.bmp");
 }
