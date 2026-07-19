500b2addeb85b83300626e8ed64dedb75655ab05
Pxdro-410 <pedrojuliocaso@gmail.com>
2026-07-18 20:34:16 -0600

    feat: modificacion para dibujar y rellenar el poligono 1 en el main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod framebuffer;
 mod line;
 mod bmp;
+mod polygon_fill;
 
 use crate::bmp::WriteBmp;
 use crate::framebuffer::Framebuffer;
-use crate::line::draw_line;
+use crate::polygon_fill::{draw_polygon, fill_polygon};
 
 fn main() {
     let mut framebuffer = Framebuffer::new(800, 600);
@@ -12,7 +13,17 @@ fn main() {
     framebuffer.set_background_color(0x000000);
     framebuffer.clear();
 
-    framebuffer.set_current_color(0xFFFFFF);
+    // Poligono 1
+    let poligon_1 = [
+        (165, 380), (185, 360), (180, 330), (207, 345), (233, 330), 
+        (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)
+    ];
+
+    // Rellenar de amarillo
+    fill_polygon(&poligon_1, &mut framebuffer, 0x00FFFF);
+    
+    // Dibujar el contorno blanco
+    draw_polygon(&poligon_1, &mut framebuffer, 0xFFFFFF);
 
     let _ = framebuffer.render_buffer("out.bmp");
 
